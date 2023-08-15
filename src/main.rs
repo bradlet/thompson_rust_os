ec6083f63e6b5b03abded11301886624292de671
Bradley Thompson <bradlet2@pdx.edu>
2023-08-15 16:34:34 -0700

    Add cargo run capability and setup custom test framework

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,26 @@
 // chain with crt0.
 #![no_main]
 
+// Enable custom test framework because the default `test` crate requires the std lib.
+#![feature(custom_test_frameworks)]
+// This generates a main function that calls `test_runner`, but we configured no_main.
+#![test_runner(crate::test_runner)]
+// This is needed to change the name of the generated main function...
+#![reexport_test_harness_main = "test_main"]
+
 use core::panic::PanicInfo;
 
 mod vga_buffer;
+
+// `tests` - slice of trait object references pointing at the 
+// [Fn()](https://doc.rust-lang.org/std/ops/trait.Fn.html) trait.
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) -> () {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -22,5 +39,10 @@ fn panic(_info: &PanicInfo) -> ! {
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
 	println!("Hello {}!", "World");
+
+	// Call the generated test main in test contexts
+	#[cfg(test)]
+	test_main();
+	
     loop {}
 }
