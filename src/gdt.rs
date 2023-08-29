c1316f86fd5a3d328d485508e0060ce3d4b5b08f
Bradley Thompson <bradlet2@pdx.edu>
2023-08-29 16:02:26 -0700

    Point to new double fault stack in IDT

--- src/gdt.rs
+++ src/gdt.rs
@@ -7,9 +7,19 @@
 use x86_64::VirtAddr;
 use x86_64::structures::tss::TaskStateSegment;
 use lazy_static::lazy_static;
-use x86_64::structures::gdt::{GlobalDescriptorTable, Descriptor};
+use x86_64::structures::gdt::{GlobalDescriptorTable, Descriptor, SegmentSelector};
 
 pub const DOUBLE_FAULT_IST_INDEX: u16 = 0;
+
+struct Selectors {
+    code_selector: SegmentSelector,
+    tss_selector: SegmentSelector,
+}
+
+struct GlobalDescriptorWrapper {
+	table: GlobalDescriptorTable,
+	selectors: Selectors
+}
 
 lazy_static! {
 	// Build out our interrupt stack table with a double fault stack impl.
@@ -33,15 +43,32 @@ lazy_static! {
 }
 
 lazy_static! {
-    static ref GDT: GlobalDescriptorTable = {
+    static ref GDT: GlobalDescriptorWrapper = {
         let mut gdt = GlobalDescriptorTable::new();
-        gdt.add_entry(Descriptor::kernel_code_segment());
-        gdt.add_entry(Descriptor::tss_segment(&TSS));
-        gdt
+
+        let code_selector = gdt.add_entry(Descriptor::kernel_code_segment());
+        let tss_selector = gdt.add_entry(Descriptor::tss_segment(&TSS));
+
+		GlobalDescriptorWrapper {
+			table: gdt,
+			selectors: Selectors { code_selector, tss_selector }
+		}
     };
 }
 
-// Same as w/ interrupts, provide a clean interface for clients.
+/// Provides a clean interface to initialize a GDT hiding unsafe
+/// operations and other lib access from clients.
 pub fn init() {
-    GDT.load();
+	use x86_64::instructions::tables::load_tss;
+    use x86_64::instructions::segmentation::{CS, Segment};
+
+    GDT.table.load();
+	unsafe {
+		// Update the code segment register, so it points
+		// at our new double fault stack.
+        CS::set_reg(GDT.selectors.code_selector);
+		// Load our Task Segment Selector so that the IDT can use
+		// it to point to the double fault stack.
+        load_tss(GDT.selectors.tss_selector);
+    }
 }
--- src/interrupts.rs
+++ src/interrupts.rs
@@ -4,7 +4,7 @@
 
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
-use crate::println;
+use crate::{println, gdt};
 use lazy_static::lazy_static;
 
 // Note to self: See [ref](https://doc.rust-lang.org/std/keyword.ref.html) docs
@@ -17,7 +17,11 @@ lazy_static! {
 	static ref IDT: InterruptDescriptorTable = {
 		let mut idt = InterruptDescriptorTable::new();
 		idt.breakpoint.set_handler_fn(breakpoint_handler);
-		idt.double_fault.set_handler_fn(double_fault_handler);
+		unsafe {
+			idt.double_fault
+				.set_handler_fn(double_fault_handler)
+				.set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX); // unsafe
+		}
 		idt
 	};
 }
